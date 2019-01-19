b0c9dd22e34886f37963e3e7c94ef9efb0d8710a
Nicola De Lazzari <nicola.delazzari@gmail.com>
2019-01-19 15:23:28 +0100

    add show_product route

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -25,7 +25,7 @@ class ProductController extends AbstractController
         $product->setPrice(1999);
         $product->setDescription('Ergonomic and stylish!');
         $product->setTag('tools');
-        $product->setCreatedAt(\DateTime::createFromFormat('Y-m-d', "2018-09-09"));
+        $product->setCreatedAt(new \DateTime());
 
         // tell Doctrine you want to (eventually) save the Product (no queries yet)
         $entityManager->persist($product);
@@ -51,6 +51,29 @@ class ProductController extends AbstractController
     }
 
     /**
+     * @Route("/product/{id}", name="product_show")
+     */
+    public function show($id)
+    {
+        $product = $this->getDoctrine()
+            ->getRepository(Product::class)
+            ->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        return new Response('Check out this great product: '.$product->getName());
+
+        // or render a template
+        // in the template, print things with {{ product.name }}
+        // return $this->render('product/show.html.twig', ['product' => $product]);
+    }
+
+
+    /**
      * @Route("/product/edit/{id}")
      */
     public function update($id)
@@ -65,6 +88,7 @@ class ProductController extends AbstractController
         }
 
         $product->setName('New product name!');
+        $product->setCreatedAt(new \DateTime());
         $entityManager->flush();
 
         return $this->redirectToRoute('product_show', [
